84d39703d59eb5eef9a793227741ab5c237bde92
Todd Perry <up612136@myport.ac.uk>
2013-02-10 19:28:01 +0000

    added skeleton for chart
    constructor, accessor, mutator, abstracts
    
    need harrys help on how to calculate timespan (i dunno how to work the
    Date class yet)

--- Application/VysiChart/src/vysichart/Chart.java
+++ Application/VysiChart/src/vysichart/Chart.java
@@ -8,6 +8,65 @@ package vysichart;
  *
  * @author Harry, Todd
  */
+import java.util.ArrayList;
+
 public abstract class Chart {
+    private float timeFrame; //total timeframe (in hrs), calculated from tasks
+    private boolean isValid; //are all the relationships valid? 
+    private int currentX;
+    private int currentY;
+    
+    public Chart(){
+        // default constructor
+        currentX = 0;
+        currentY = currentX; // coords default as 0, 0
+        isValid = true; // default as true
+        
+    }
+    
+    // --- Accessors ---
+    
+    public float getTimeFrame(){ // only accessor, no mutator, as it's calculated
+        return timeFrame;
+    }
+    
+    public boolean getValidity(){
+        return isValid; // only accessor, no mutator, as it's calculated
+    }
+    
+    public int getCurrentX(){
+        return currentX;
+    }
+    
+    public int getCurrentY(){
+        return currentY;
+    }
+    
+    // --- Mutators ---
+    
+    public void setCurrentX(int newX){
+        currentX = newX;
+    }
+    
+    public void setCurrentY(int newY){
+        currentY = newY;
+    }
+    
+    public void setCoords(int newX, int newY){
+        currentX = newX;
+        currentY = newY;
+    }
+    
+    // --- Abstract Methods ---
+    
+    protected abstract void drawNode();
+    
+    protected abstract void drawChart();
+    
+    protected abstract int[] calculateNextCoords(); // calls calculate nX and calculate nY
+    
+    protected abstract int calculateNextX();
+    
+    protected abstract int calculateNextY();
     
 }
